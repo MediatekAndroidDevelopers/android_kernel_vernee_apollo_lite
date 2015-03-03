61ab0384f3517f265416bc197ff112433adce0e9
Dylan Reid <dgreid@chromium.org>
2015-03-02 17:09:07 -0800

    ANDROID: mmc: Move tracepoint creation and export symbols
    Move the tracepoint creation to core from card, as core shouldn't depend
    on card.
    
    Also add EXPORT_SYMBOL_GPL calls to enable module build.
    
    Change-Id: Ie39fcdadc0516df99600d0963efe09b6cd7a9bf8
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    (cherry picked from commit da5fbd1e7e50fee3a8271f50d25c848d0ede64b3,
    from android-3.14)
    Signed-off-by: Brian Norris <briannorris@google.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -30,6 +30,7 @@
 #include <linux/slab.h>
 #include <linux/of.h>
 
+#define CREATE_TRACE_POINTS
 #include <trace/events/mmc.h>
 
 #include <linux/mmc/card.h>
@@ -50,6 +51,11 @@
 #include "sd_ops.h"
 #include "sdio_ops.h"
 #include "../card/mt_mmc_block.h"
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(mmc_blk_erase_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(mmc_blk_erase_end);
+EXPORT_TRACEPOINT_SYMBOL_GPL(mmc_blk_rw_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(mmc_blk_rw_end);
 
 /* If the device is not responding */
 #define MMC_CORE_TIMEOUT_MS	(10 * 60 * 1000) /* 10 minute timeout */
