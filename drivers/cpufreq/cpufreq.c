9104629cc483b90bef13ccd8cfe85464997e596f
Juri Lelli <juri.lelli@arm.com>
2015-04-30 17:35:23 +0100

    DEBUG: sched,cpufreq: add cpu_capacity change tracepoint
    This is useful when we want to compare cpu utilization and
    cpu curr capacity side by side.
    
    Change-Id: Icd0930d11068fcb7d2b6a9a48e7ed974904e1081
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -28,6 +28,7 @@
 #include <linux/slab.h>
 #include <linux/suspend.h>
 #include <linux/tick.h>
+#include <linux/sched.h>
 #include <trace/events/power.h>
 
 #ifdef CONFIG_ARCH_MT6797
@@ -422,6 +423,7 @@ static void cpufreq_notify_post_transition(struct cpufreq_policy *policy,
 void cpufreq_freq_transition_begin(struct cpufreq_policy *policy,
 		struct cpufreq_freqs *freqs)
 {
+	int cpu;
 
 	/*
 	 * Catch double invocations of _begin() which lead to self-deadlock.
@@ -450,6 +452,8 @@ wait:
 	spin_unlock(&policy->transition_lock);
 
 	scale_freq_capacity(policy, freqs);
+	for_each_cpu(cpu, policy->cpus)
+		trace_cpu_capacity(capacity_curr_of(cpu), cpu);
 
 	cpufreq_notify_transition(policy, freqs, CPUFREQ_PRECHANGE);
 }
