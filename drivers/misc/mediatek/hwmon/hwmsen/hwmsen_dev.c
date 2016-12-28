cd96f4621e5c0d58454b061bd99926eeb8094c22
fire855 <thefire855@gmail.com>
2016-12-28 01:57:19 +0100

    Revert "Fix "buffer overflow in Mediatek HWMON driver" issue"
    This reverts commit d9bc1f853c4ee6e713f9b80dcbb45f061a37f95c.

--- drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
+++ drivers/misc/mediatek/hwmon/hwmsen/hwmsen_dev.c
@@ -594,11 +594,6 @@ static int hwmsen_enable(struct hwmdev_object *obj, int sensor, int enable)
 
 	sensor_type = 1LL << sensor;
 
-	if (sensor > MAX_ANDROID_SENSOR_NUM) {
-		HWM_ERR("handle %d!\n", sensor);
-		return -EINVAL;
-	}
-
 	if (!obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
 		return -EINVAL;
@@ -717,11 +712,6 @@ static int hwmsen_enable_nodata(struct hwmdev_object *obj, int sensor, int enabl
 	HWM_FUN(f);
 	sensor_type = 1LL << sensor;
 
-	if (sensor > MAX_ANDROID_SENSOR_NUM) {
-		HWM_ERR("handle %d!\n", sensor);
-		return -EINVAL;
-	}
-
 	if (NULL == obj) {
 		HWM_ERR("hwmdev obj pointer is NULL!\n");
 		return -EINVAL;
@@ -784,7 +774,6 @@ static int hwmsen_set_delay(int delay, int handle)
 		HWM_ERR("handle %d!\n", handle);
 		return -EINVAL;
 	}
-
 	cxt = hwm_obj->dc->cxt[handle];
 	if (NULL == cxt || (cxt->obj.sensor_operate == NULL)) {
 		HWM_ERR("have no this sensor %d or operator point is null!\r\n", handle);
