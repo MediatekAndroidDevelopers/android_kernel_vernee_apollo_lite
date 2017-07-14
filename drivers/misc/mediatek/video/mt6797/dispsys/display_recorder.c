d6c6714ece10d50cc7a283dbd32b88c61aa63a0d
fire855 <thefire855@gmail.com>
2017-07-14 21:22:32 +0200

    Compiling fixes

--- drivers/misc/mediatek/video/mt6797/dispsys/display_recorder.c
+++ drivers/misc/mediatek/video/mt6797/dispsys/display_recorder.c
@@ -198,12 +198,14 @@ static unsigned int analysize_length;
 
 char dprec_error_log_buffer[DPREC_ERROR_LOG_BUFFER_LENGTH];
 static dprec_logger_event dprec_vsync_irq_event;
+#ifdef CONFIG_TRACING
 static met_log_map dprec_met_info[DISP_SESSION_MEMORY + 2] = {
 	{"UNKWON", 0, 0},
 	{"OVL0-DSI", 0, 0},
 	{"OVL1-MHL", 0, 0},
 	{"OVL1-SMS", 0, 0},
 };
+#endif
 
 int dprec_init(void)
 {
