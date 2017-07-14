2d1509e253c0e44a208d4e30db79415bdb122501
fire855 <thefire855@gmail.com>
2017-07-14 22:30:37 +0200

    Fix "Elevation of privilege vulnerability in MediaTek components"
    CVE-2017-0502

--- drivers/misc/mediatek/m4u/2.0/m4u.c
+++ drivers/misc/mediatek/m4u/2.0/m4u.c
@@ -2014,7 +2014,7 @@ static long MTK_M4U_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
 	int ret = 0;
 	M4U_MOUDLE_STRUCT m4u_module;
 	M4U_PORT_STRUCT m4u_port;
-	M4U_PORT_ID PortID;
+	/*M4U_PORT_ID PortID;*/
 	M4U_PORT_ID ModuleID;
 	M4U_CACHE_STRUCT m4u_cache_data;
 	M4U_DMA_STRUCT m4u_dma_data;
@@ -2123,6 +2123,7 @@ static long MTK_M4U_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
 		mutex_unlock(&gM4u_sec_init);
 #endif
 		break;
+#if 0
 	case MTK_M4U_T_MONITOR_START:
 		ret = copy_from_user(&PortID, (void *)arg, sizeof(unsigned int));
 		if (ret) {
@@ -2132,6 +2133,7 @@ static long MTK_M4U_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
 		ret = m4u_monitor_start(m4u_port_2_m4u_id(PortID));
 
 		break;
+
 	case MTK_M4U_T_MONITOR_STOP:
 		ret = copy_from_user(&PortID, (void *)arg, sizeof(unsigned int));
 		if (ret) {
@@ -2140,6 +2142,7 @@ static long MTK_M4U_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
 		}
 		ret = m4u_monitor_stop(m4u_port_2_m4u_id(PortID));
 		break;
+#endif
 	case MTK_M4U_T_CACHE_FLUSH_ALL:
 		m4u_dma_cache_flush_all();
 		break;
